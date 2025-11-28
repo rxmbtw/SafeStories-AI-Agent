c49cae287435b78143afa1889660ce9001542a9e
rxmbtw <ramsurse2@gmail.com>
2025-11-28 13:38:22 +0530

    Fix Vercel deployment - use responsive index.html

--- vite.config.responsive.js
+++ vite.config.responsive.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index-responsive.html'
-      }
-    }
-  }
+  plugins: [react()]
 })
